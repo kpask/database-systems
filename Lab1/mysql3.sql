da3a898b6de55a1e6b0f16e4246925eb74f2d747
Karolis Paskevicius <kp704595@gmail.com>
2025-10-12 10:49:56 +0300

    Atnaujinta mysql3 uzklausa pagal nauja prasyma

--- Lab1/mysql3.sql
+++ Lab1/mysql3.sql
@@ -1,9 +1,14 @@
 -- Kiekvienai datai, kada buvo paimta bent viena knyga
--- Visu paimtu skirtingu knygu ir visu ju egzemplioriu skaiciai
+-- Visu paimtu skirtingu knygu ir visu ju egzemplioriu skaiciai, paemimu skaicius
+-- datos kai paemimu skaicius lygus skaitytoju skaiciui
 SELECT s.paimta,
 COUNT(DISTINCT e.isbn) AS skirtingu_paimtu_skaicius,
-COUNT(DISTINCT s.egzempliorius) as paimtu_skaicius
+COUNT(DISTINCT s.egzempliorius) as paimtu_egzemplioriu_skaicius,
+COUNT(*) as paemimu_skaicius,
+COUNT(DISTINCT s.skaitytojas) as skaitytoju_skaicius
 FROM stud.skaitymas s
 JOIN stud.egzempliorius e ON s.egzempliorius = e.nr
 GROUP BY s.paimta
+HAVING COUNT(*) = COUNT(DISTINCT s.skaitytojas)
 ORDER BY s.paimta DESC;
+
